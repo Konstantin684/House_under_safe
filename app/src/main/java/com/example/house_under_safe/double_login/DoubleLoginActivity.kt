a8c21eb69e3dd9ddad1a94f5606064c9632ff4ac
Konstantin684 <kostazakirov30@gmail.com>
2025-04-05 21:59:48 +0300

    Добавлен новый экран двухфакторной авторизации

---
+++ app/src/main/java/com/example/house_under_safe/double_login/DoubleLoginActivity.kt
@@ -0,0 +1,22 @@
+package com.example.house_under_safe.double_login
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.house_under_safe.R
+
+class DoubleLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_double_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+}
